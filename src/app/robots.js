a6a53bcdf9e41ee7ac73131278bc4a84416394d5
Milad Arbabi <arbabi.milad@gmail.com>
2025-08-21 14:53:45 +0200

    feat: maintenance robots + noindex for coming-soon

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,6 +1,18 @@
+// src/app/robots.js
 export default function robots() {
+  const comingSoon = process.env.NEXT_PUBLIC_COMING_SOON === '1';
+
+  if (comingSoon) {
+    // Maintenance mode: block crawling, omit sitemap
+    return {
+      rules: [{ userAgent: '*', disallow: '/' }],
+      sitemap: undefined,
+    };
+  }
+
+  // Normal mode: allow crawling + expose sitemap
   return {
     rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://auvra.shop/sitemap.xml'
+    sitemap: 'https://auvra.shop/sitemap.xml',
   };
 }
